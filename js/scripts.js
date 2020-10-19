ae04359bdf924361423f8757f8e9918f6c25f418
Eric Kibogo <erickibogo216@gmail.com>
2020-10-20 00:45:25 +0300

    Add 'add another pizza'  button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -80,5 +80,21 @@ $(document).ready(function(){
         $("div.tabulation").toggle();
       }
 
+      $("#pizzaname").html($(".name option:selected").val());
+      $("#pizzasize").html( $("#size option:selected").val());
+      $("#pizzacrust").html($("#crust option:selected").val());
+      $("#pizzatopping").html(ptopping.join(", "));
+      $("#totals").html(total);
+      
+  
+      $("button.addPizza").click(function(){
+        let pname = $(".name option:selected").val();
+        let psize = $("#size option:selected").val();
+        let pcrust = $("#crust option:selected").val();
+        let ptopping = [];
+        $.each($("input[name='toppings']:checked"), function(){            
+            ptopping.push($(this).val());
+        });
+
     
     
