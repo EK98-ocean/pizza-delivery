5f8c734a7b484d974e1f3d3a019fde430e43d9cd
Eric Kibogo <erickibogo216@gmail.com>
2020-10-19 21:43:12 +0300

    Add stylesheets, scripts to HTML

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// business logic
+function Pizza(name, size, topping, crust, ) {
+    this.name = name;
+    this.size = size;
+    this.crust = crust;
+    this.topping 
+    this.
+  }
+
+switch(size{}
+
+$(document).ready
